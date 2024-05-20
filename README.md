20abfc8cf311ef13ee169a30b5b81e9cd6ae9987
snowAvacado <nikhil.mara94@icloud.com>
2024-05-20 18:39:30 +0200

    clean comments

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This repo contains my code for "Build your own HTTP server challenge" on code crafters.
 (https://app.codecrafters.io/users/snowAvacado)
 
+
 To run server code
 ```bash
 ./my_server.sh
--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# Uncomment this to pass the first stage
 import socket
 import re
 import sys 
@@ -7,11 +6,6 @@ import gzip
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-  print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
   server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
   while True:
     conn, addr = server_socket.accept() # wait for client
